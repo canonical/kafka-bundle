eeed25f682be661978e4e2377aec51f5184c446f
Jarred Wilson <jarred.wilson12@gmail.com>
2022-09-06 18:00:15 -0400

    Add scaling tests

---
+++ tests/integration/test_scaling.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import asyncio
+import logging
+import time
+from pathlib import Path
+
+import pytest
+import yaml
+from pytest_operator.plugin import OpsTest
+
+from tests.integration.zookeeper_helpers import (
+    check_key,
+    get_password,
+    ping_servers,
+    restart_unit,
+    write_key,
+)
+
+logger = logging.getLogger(__name__)
+
+APPLICATIONS = ["zookeeper", "kafka"]
+BUNDLE="releases/latest/k8s/kafka-k8s.yaml"
+KUBERNETES=True if yaml.safe_load(Path(BUNDLE).read_text())["bundle"] == "kubernetes" else False 
+
+
+@pytest.mark.abort_on_fail
+async def test_deploy_active(ops_test: OpsTest):
+    charm = await ops_test.deploy_bundle(bundle=BUNDLE, build=False)
+    time.sleep(60)
+    await ops_test.model.block_until(lambda: (len(ops_test.model.applications["zookeeper"].units) == 3 and len(ops_test.model.applications["kafka"].units) == 3))
+    await ops_test.model.set_config({"update-status-hook-interval": "10s"})
+    await ops_test.model.wait_for_idle(apps=APPLICATIONS, status="active", timeout=1000)
+
+    for app in APPLICATIONS:
+        assert ops_test.model.applications[app].status == "active"
+
+    await ops_test.model.set_config({"update-status-hook-interval": "60m"})
+
+
+@pytest.mark.abort_on_fail
+async def test_zookeeper_simple_scale_up(ops_test: OpsTest):
+    await ops_test.model.applications["zookeeper"].add_units(count=3)
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications["zookeeper"].units) == 6)
+    await ops_test.model.wait_for_idle(apps=["zookeeper"], status="active", timeout=1000)
+    assert ping_servers(ops_test)
+
+
+@pytest.mark.abort_on_fail
+async def test_zookeeper_simple_scale_down(ops_test: OpsTest):
+    if not KUBERNETES:
+        await ops_test.model.applications["zookeeper"].destroy_units(
+            f"{'zookeeper'}/5", f"{'zookeeper'}/4", f"{'zookeeper'}/3"
+        )
+    else:
+        await ops_test.model.applications["zookeeper"].scale(scale_change=-3)
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications["zookeeper"].units) == 3)
+    await ops_test.model.wait_for_idle(apps=["zookeeper"], status="active", timeout=1000)
+    assert ping_servers(ops_test)
+
+
+@pytest.mark.abort_on_fail
+async def test_zookeeper_scale_up_replication(ops_test: OpsTest):
+    await ops_test.model.wait_for_idle(apps=["zookeeper"], status="active", timeout=1000)
+    assert ping_servers(ops_test)
+    host = ops_test.model.applications["zookeeper"].units[0].public_address
+    model_full_name = ops_test.model_full_name
+    password = get_password(model_full_name or "")
+    write_key(host=host, password=password)
+    await ops_test.model.applications["zookeeper"].add_units(count=1)
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications["zookeeper"].units) == 4)
+    await ops_test.model.wait_for_idle(apps=["zookeeper"], status="active", timeout=1000)
+    check_key(host=host, password=password)
+
+
+@pytest.mark.abort_on_fail
+async def test_zookeeper_kill_quorum_leader_remove(ops_test: OpsTest):
+    """Gracefully removes ZK quorum leader using `juju remove`."""
+    if not KUBERNETES:
+        await ops_test.model.set_config({"update-status-hook-interval": "1m"})
+        await ops_test.model.applications["zookeeper"].destroy_units(f"{'zookeeper'}/0")
+        await ops_test.model.block_until(lambda: len(ops_test.model.applications["zookeeper"].units) == 3)
+        await ops_test.model.wait_for_idle(apps=["zookeeper"], status="active", timeout=1000)
+        assert ping_servers(ops_test)
+        await ops_test.model.set_config({"update-status-hook-interval": "60m"})
+
+
+@pytest.mark.abort_on_fail
+async def test_zookeeper_kill_juju_leader_remove(ops_test: OpsTest):
+    """Gracefully removes Juju leader using `juju remove`."""
+    if not KUBERNETES:
+        await ops_test.model.set_config({"update-status-hook-interval": "1m"})
+        leader = None
+        for unit in ops_test.model.applications["zookeeper"].units:
+            if await unit.is_leader_from_status():
+                leader = unit.name
+                break
+
+        if leader:
+            await ops_test.model.applications["zookeeper"].destroy_units(leader)
+            await ops_test.model.block_until(
+                lambda: len(ops_test.model.applications["zookeeper"].units) == 2
+            )
+            await ops_test.model.wait_for_idle(apps=["zookeeper"], status="active", timeout=1000)
+            assert ping_servers(ops_test)
+        await ops_test.model.set_config({"update-status-hook-interval": "60m"})
+
+
+@pytest.mark.abort_on_fail
+async def test_zookeeper_kill_juju_leader_restart(ops_test: OpsTest):
+    """Rudely removes Juju leader by restarting the LXD container."""
+    if not KUBERNETES:
+        await ops_test.model.set_config({"update-status-hook-interval": "1m"})
+        leader = None
+        for unit in ops_test.model.applications["zookeeper"].units:
+            if await unit.is_leader_from_status():
+                leader = unit.name
+                break
+
+        if leader:
+            # adding another unit to ensure minimum units for quorum
+            await ops_test.model.applications["zookeeper"].add_units(count=1)
+            await ops_test.model.block_until(
+                lambda: len(ops_test.model.applications["zookeeper"].units) == 3
+            )
+            await ops_test.model.wait_for_idle(apps=["zookeeper"], status="active", timeout=1000)
+
+            model_full_name = ops_test.model_full_name
+            if model_full_name:
+                restart_unit(model_full_name=model_full_name, unit=leader)
+                time.sleep(10)
+                assert ping_servers(ops_test)
+            else:
+                raise
+
+
+@pytest.mark.abort_on_fail
+async def test_zookeeper_same_model_application_deploys(ops_test: OpsTest):
+    """Ensures that re-deployments of the charm starts on the same model."""
+    await asyncio.gather(ops_test.model.applications["zookeeper"].remove())
+    time.sleep(60)
+    zoo = await ops_test.model.deploy("zookeeper", application_name="zookeeper", num_units=3)
+    await zoo.add_relation("zookeeper", "kafka")
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications["zookeeper"].units) == 3)
+    await ops_test.model.set_config({"update-status-hook-interval": "10s"})
+    await ops_test.model.wait_for_idle(apps=["zookeeper"], status="active", timeout=1000)
+
+    assert ops_test.model.applications["zookeeper"].status == "active"
+
+    await ops_test.model.set_config({"update-status-hook-interval": "60m"})
+
+
+@pytest.mark.abort_on_fail
+async def test_kafka_simple_scale_up(ops_test: OpsTest):
+    await ops_test.model.applications["kafka"].add_units(count=2)
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications["kafka"].units) == 3)
+    await ops_test.model.wait_for_idle(apps=["kafka"], status="active", timeout=1000)
+
+    kafka_zk_relation_data = get_kafka_zk_relation_data(
+        unit_name="kafka/2", model_full_name=ops_test.model_full_name
+    )
+    active_brokers = get_active_brokers(zookeeper_config=kafka_zk_relation_data)
+    chroot = kafka_zk_relation_data.get("chroot", "")
+    assert f"{chroot}/brokers/ids/0" in active_brokers
+    assert f"{chroot}/brokers/ids/1" in active_brokers
+    assert f"{chroot}/brokers/ids/2" in active_brokers
+
+
+@pytest.mark.abort_on_fail
+async def test_kafka_simple_scale_down(ops_test: OpsTest):
+    if not KUBERNETES:
+        await ops_test.model.applications["kafka"].destroy_units(f"{'kafka'}/1")
+        await ops_test.model.block_until(lambda: len(ops_test.model.applications["kafka"].units) == 2)
+        await ops_test.model.wait_for_idle(apps=["kafka"], status="active", timeout=1000)
+
+        time.sleep(30)
+
+        kafka_zk_relation_data = get_kafka_zk_relation_data(
+            unit_name="kafka/2", model_full_name=ops_test.model_full_name
+        )
+        active_brokers = get_active_brokers(zookeeper_config=kafka_zk_relation_data)
+        chroot = kafka_zk_relation_data.get("chroot", "")
+        assert f"{chroot}/brokers/ids/0" in active_brokers
+        assert f"{chroot}/brokers/ids/1" not in active_brokers
+        assert f"{chroot}/brokers/ids/2" in active_brokers
---
+++ tests/integration/zookeeper_helpers.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import re
+from pathlib import Path
+from subprocess import PIPE, check_output
+from typing import Dict
+
+import yaml
+from kazoo.client import KazooClient
+from pytest_operator.plugin import OpsTest
+
+APP_NAME = "zookeeper"
+
+
+def get_password(model_full_name: str) -> str:
+    # getting relation data
+    show_unit = check_output(
+        f"JUJU_MODEL={model_full_name} juju show-unit {APP_NAME}/0",
+        stderr=PIPE,
+        shell=True,
+        universal_newlines=True,
+    )
+    response = yaml.safe_load(show_unit)
+    relations_info = response[f"{APP_NAME}/0"]["relation-info"]
+
+    for info in relations_info:
+        if info["endpoint"] == "cluster":
+            password = info["application-data"]["super-password"]
+            return password
+    else:
+        raise Exception("no relations found")
+
+
+def restart_unit(model_full_name: str, unit: str) -> None:
+    # getting juju id
+    machine_id = check_output(
+        f"JUJU_MODEL={model_full_name} juju status | grep {unit} | awk '{{ print $4 }}'",
+        stderr=PIPE,
+        shell=True,
+        universal_newlines=True,
+    )
+
+    # getting lxc machine name
+    machine_name = check_output(
+        f"JUJU_MODEL={model_full_name} juju machines | grep awk '{{print $4}}' | grep -e '-{machine_id}'| head -1",
+        stderr=PIPE,
+        shell=True,
+        universal_newlines=True,
+    )
+    _ = check_output(
+        f"lxc restart {machine_name}",
+        stderr=PIPE,
+        shell=True,
+        universal_newlines=True,
+    )
+
+
+def write_key(host: str, password: str, username: str = "super") -> None:
+    kc = KazooClient(
+        hosts=host,
+        sasl_options={"mechanism": "DIGEST-MD5", "username": username, "password": password},
+    )
+    kc.start()
+    kc.create_async("/legolas", b"hobbits")
+    kc.stop()
+    kc.close()
+
+
+def check_key(host: str, password: str, username: str = "super") -> None:
+    kc = KazooClient(
+        hosts=host,
+        sasl_options={"mechanism": "DIGEST-MD5", "username": username, "password": password},
+    )
+    kc.start()
+    assert kc.exists_async("/legolas")
+    value, _ = kc.get_async("/legolas") or None, None
+
+    stored_value = ""
+    if value:
+        stored_value = value.get()
+    if stored_value:
+        assert stored_value[0] == b"hobbits"
+        return
+
+    raise KeyError
+
+
+def srvr(host: str) -> Dict:
+    """Retrieves attributes returned from the 'srvr' 4lw command.
+
+    Specifically for this test, we are interested in the "Mode" of the ZK server,
+    which allows checking quorum leadership and follower active status.
+    """
+    response = check_output(
+        f"echo srvr | nc {host} 2181", stderr=PIPE, shell=True, universal_newlines=True
+    )
+
+    result = {}
+    for item in response.splitlines():
+        k = re.split(": ", item)[0]
+        v = re.split(": ", item)[1]
+        result[k] = v
+
+    return result
+
+
+async def ping_servers(ops_test: OpsTest) -> bool:
+    for unit in ops_test.model.applications[APP_NAME].units:
+        host = unit.public_address
+        mode = srvr(host)["Mode"]
+        if mode not in ["leader", "follower"]:
+            return False
+
+    return True
+
+
+def check_jaas_config(model_full_name: str, unit: str):
+    config = check_output(
+        f"JUJU_MODEL={model_full_name} juju exec cat /var/snap/kafka/common/zookeeper-jaas.cfg --unit {unit}",
+        stderr=PIPE,
+        shell=True,
+        universal_newlines=True,
+    )
+
+    user_lines = {}
+    for line in config.splitlines():
+        matched = re.search(pattern=r"user_([a-zA-Z\-\d]+)=\"([a-zA-Z0-9]+)\"", string=line)
+        if matched:
+            user_lines[matched[1]] = matched[2]
+
+    return user_lines
