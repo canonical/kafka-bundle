f166033fe0ec4bf32964a14aaf268986261c20d9
Jarred Wilson <jarred.wilson12@gmail.com>
2022-09-13 11:47:47 -0400

    Add action workflows

--- update_bundle.py
+++ update_bundle.py
@@ -22,20 +22,6 @@ def fetch_revision(charm, charm_channel):
             return channel["revision"]["revision"]
     raise ValueError("Revision not found.")
 
-
-def has_revision_update(bundle_path):
-    """Returns true if updated revision is available."""
-    bundle_data = yaml.safe_load(Path(bundle_path).read_text())
-    for app in bundle_data["applications"]:
-        if bundle_data["applications"][app]["revision"] != fetch_revision(
-            bundle_data["applications"][app]["charm"],
-            bundle_data["applications"][app]["channel"],
-        ):
-            return True
-        else:
-            return False
-
-
 def update_bundle(bundle_path):
     """Updates a bundle's revision number."""
     bundle_data = yaml.safe_load(Path(bundle_path).read_text())
@@ -49,4 +35,5 @@ def update_bundle(bundle_path):
         bundle.close()
 
 
-update_bundle(sys.argv[1])
+if __name__ == '__main__':
+    update_bundle(sys.argv[1])
