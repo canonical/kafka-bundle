4cdd14affa7ae6eb7b89565f501a5735d40cc9ad
deusebio <enrico.deusebio@canonical.com>
2023-05-25 09:40:43 +0200

    [MISC] Makefile refactoring (#59)

--- tests/integration/bundle/test_bundle.py
+++ tests/integration/bundle/test_bundle.py
@@ -3,6 +3,7 @@
 # See LICENSE file for licensing details.
 
 import logging
+import os
 from pathlib import Path
 
 import pytest
@@ -17,6 +18,7 @@ from tests.integration.bundle.kafka_helpers import (
 )
 from tests.integration.bundle.literals import (
     APP_CHARM_PATH,
+    BUNDLE_BUILD,
     BUNDLE_PATH,
     KAFKA,
     TLS_PORT,
@@ -40,8 +42,11 @@ async def test_deploy_bundle_active(ops_test: OpsTest):
     for app in bundle_data["applications"]:
         applications.append(app)
 
+    file = BUNDLE_BUILD if os.path.exists(f"./{BUNDLE_BUILD}") else BUNDLE_PATH
+
+    logger.info(f"Deploying Bundle with file {file}")
     retcode, stdout, stderr = await ops_test.run(
-        *["juju", "deploy", "--trust", "-m", ops_test.model_full_name, f"./{BUNDLE_PATH}"]
+        *["juju", "deploy", "--trust", "-m", ops_test.model_full_name, f"./{file}"]
     )
     assert retcode == 0, f"Deploy failed: {(stderr or stdout).strip()}"
     logger.info(stdout)
