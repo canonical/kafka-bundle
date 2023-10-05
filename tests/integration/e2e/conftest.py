42f8ec4fea9af65dc47e2e8ee39e1b620787f008
Enrico Deusebio <enrico.deusebio@canonical.com>
2023-10-05 21:23:12 +0200

    [DPE-2696] Update tls integration tests to legacy

--- tests/integration/e2e/conftest.py
+++ tests/integration/e2e/conftest.py
@@ -137,7 +137,7 @@ async def deploy_cluster(ops_test: OpsTest, tls):
             application_name=TLS_CHARM_NAME,
             num_units=1,
             series="jammy",
-            channel="beta",
+            channel="legacy/stable",
             config={"generate-self-signed-certificates": "true", "ca-common-name": "Canonical"},
         )
         await ops_test.model.wait_for_idle(apps=[TLS_CHARM_NAME])
