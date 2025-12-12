12b752cd8ae2baa2813bb918ed50e203806ec075
Iman Enami <iman.enami@canonical.com>
2025-12-12 09:19:28 +0100

    fix karapace TLS test issue

--- tests/integration/terraform/component_validation.py
+++ tests/integration/terraform/component_validation.py
@@ -134,12 +134,18 @@ class ComponentValidation:
 
     def test_karapace(self):
         """Test creating a schema subject in Karapace, listing it and then deletes it."""
+        scheme = "https" if self.tls else "http"
         schema_name = "test-key"
         result = self.juju.run(unit=self.karapace_unit_name, action="get-password")
         password = result.results.get("password")
         karapace_endpoint = self.get_karapace_endpoint()
-        base_url = f"http://{karapace_endpoint}"
+        base_url = f"{scheme}://{karapace_endpoint}"
         auth = ("operator", password)
+
+        # Verify that we're using TLS provider's cert in case TLS enabled.
+        _verify = False
+        if self.tls:
+            _verify = CA_FILE
 
         # Create the schema
         schema_data = {
@@ -151,6 +157,7 @@ class ComponentValidation:
             json=schema_data,
             headers={"Content-Type": "application/vnd.schemaregistry.v1+json"},
             auth=auth,
+            verify=_verify,
         )
         response.raise_for_status()
         result = response.text
@@ -163,6 +170,7 @@ class ComponentValidation:
         response = requests.get(
             f"{base_url}/subjects",
             auth=auth,
+            verify=_verify,
         )
         response.raise_for_status()
         result = response.text
@@ -173,6 +181,7 @@ class ComponentValidation:
         response = requests.delete(
             f"{base_url}/subjects/{schema_name}",
             auth=auth,
+            verify=_verify,
         )
         response.raise_for_status()
 
